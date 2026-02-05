bb330987133ef5c34d60b0b584a3f713ea0d829d
Ꭺ꯭ᏼ꯭ʜ༑꯭ㅤ꯭у꯭ᴀᴅ꯭ᴀ꯭ꪜ✨ <avtaryadavchungi@gmail.com>
2026-02-05 15:22:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/viscity/Abhimusic.git",
+    "https://github.com/ioxabhi/aaru.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
